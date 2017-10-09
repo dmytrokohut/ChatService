cface6c82d7792dbdfac3c9c2013fdf7c868f71e
Dima Kohut <retr0finr1r@gmail.com>
2017-10-09 20:45:05 +0300

    Added User entity, ticket #1

---
+++ src/main/java/com/dkohut/chatservice/common/entity/User.java
@@ -0,0 +1,80 @@
+package com.dkohut.chatservice.common.entity;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+/**
+ * 
+ * @author Dmytro Kohut
+ *
+ */
+public class User {
+
+	private Integer id;
+	private String name;
+	private String login;
+	private String password;
+	private String email;
+	
+	/**
+	 * This constructor will be used when receiving an object from HTTP-request.
+	 */
+	public User() {
+		// default constructor
+	}
+	
+	/**
+	 * This constructor will be used in DAO methods.
+	 * 
+	 * @param ResultSet resultSet
+	 * @throws SQLException 
+	 */
+	public User(ResultSet resultSet) throws SQLException {
+		this.id = resultSet.getInt("id");
+		this.name = resultSet.getString("name");
+		this.login = resultSet.getString("login");
+		this.password = resultSet.getString("password");
+		this.email = resultSet.getString("email");
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getLogin() {
+		return login;
+	}
+
+	public void setLogin(String login) {
+		this.login = login;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	
+}
